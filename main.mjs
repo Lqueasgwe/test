bf6cdde439ce0f524778455706c9dda0d58336dd
Ruslan <wndrchild@gmail.com>
2020-02-27 15:52:16 +0200

    Add files via upload

---
+++ main.mjs
@@ -0,0 +1,42 @@
+/*
+// Задача 0
+
+ let myName = 'Rslan';
+ let mySurname = 'Merekha';
+ let myCity = 'Kharkiv';
+
+var myUniversity = 'NURE';
+var theWeather = 'Sunny';
+var mySchool = 'LemonSchool';
+ 
+ const myAge = '20';
+ const birthday = '26.10';
+ const myBroAge = '23';
+
+ // Задача 1
+
+ let firstNumber = 21;
+ let secondNumber = 36;
+   if (firstNumber % 2 != 0 ) {
+      console.log(firstNumber);
+   }
+  
+    if (secondNumber % 2 != 0 ) {
+      console.log(secondNumber);
+    }
+   
+    // Задача 2
+   
+
+   let year = prompt('Введите год', '');
+     if (year % 4 != 0 && year % 400 != 0) {
+       alert ('Это невисокостный год - 365 дней');
+     }
+      else {
+       alert ('В этом году 366 дней');
+      }
+      */
+    
+      // Задача 3
+       
+    
